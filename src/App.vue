a9f5804d88ec6b5801f66e387baf5550868b32b4
Tracy <kiki20100908@163.com>
2022-05-09 11:55:12 +0800

    feat: toggle aside

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
-<script setup>
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-</script>
-
 <template>
   <router-view />
 </template>
 
-<style>
-#app {
-  /* font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50; */
-  /* margin-top: 60px; */
+<script lang="ts">
+import { provide, ref } from 'vue'
+export default {
+  name: 'App',
+  setup() {
+    const asideVisible = ref(false)
+    provide('asideVisible', asideVisible)
+  }
 }
-</style>
+</script>
