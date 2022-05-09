e6fcc31e0a826caa9d17bfd543a573e45349ab44
Tracy <kiki20100908@163.com>
2022-05-09 15:34:26 +0800

    feat: 完善 aside toggle

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="topnav">
-        <div class="logo" @click="toggleAside">LOGO</div>
+        <div class="logo">LOGO</div>
         <ul class="menu">
             <li>菜单一</li>
             <li>菜单二</li>
         </ul>
+        <span class="toggleAside" @click="toggleAside"></span>
     </div>
 </template>
 
@@ -38,6 +39,27 @@ export default {
         flex-wrap: nowrap;
         > li {
             margin: 0 1em;
+        }
+    }
+    .toggleAside {
+        width: 24px;
+        height: 24px;
+        background: red;
+        position: absolute;
+        left: 16px;
+        top: 50%;
+        transform: translateY(-50%);
+        display: none;
+    }
+    @media (max-width: 500px) {
+        > .menu {
+            display: none;
+        }
+        > .logo {
+            margin: 0 auto;
+        }
+        > .toggleAside {
+            display: inline-block;
         }
     }
 }
